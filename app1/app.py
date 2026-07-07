bf33215acf156ffa99758395880c9a2c56bae6c5
sonawanedeepak221 <sonawanedeepak221@gmail.com>
2026-07-07 20:01:51 +0530

    Update app.py

--- app1/app.py
+++ app1/app.py
@@ -9,4 +9,7 @@ def home():
               <img src='static/images/flask-badge-1.png'/>"""
 
 if __name__ == '__main__':
-    app.run() 
+    app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 8080))
+)
